b7aa8928ed22f9e6091006da63a1744aad71fed8
WalterX95 <walter_1995@hotmail.it>
2024-11-13 16:50:12 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+const inputToDoList = document.getElementById("todoContent");
+const btnToDoList = document.getElementById("todoCreate");
+const dataList = document.getElementById("list");
+const arrayList = [];
+
+btnToDoList.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (!check()) return;
+    addArray();
+    printData();
+    inputToDoList.value = '';
+});
+
+function check() {
+    if (inputToDoList.value === '') {
+        alert('Form Non valido');
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function addArray() {
+    arrayList.push(inputToDoList.value);
+}
+
+function printData() {
+    list.innerHTML = '';
+    for (let i = 0; i < arrayList.length; i++) {
+        let newLi = document.createElement('li');
+        newLi.innerText = arrayList[i];
+        list.appendChild(newLi);
+        let btncanc = document.createElement('button');
+        btncanc.setAttribute('type', 'button');
+        btncanc.setAttribute('onclick', `deleteItem(${i})`);
+        btncanc.innerText = '🗑️';
+        newLi.appendChild(btncanc);
+    }
+}
+
+function deleteItem(index) {
+    arrayList.splice(index, 1);
+    printData();
+}
